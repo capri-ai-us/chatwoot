35d0e2533322a6787f490de55ec181dbfe7441e3
capri-ai-us <69325480+capri-ai-us@users.noreply.github.com>
2021-12-17 14:54:44 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,11 @@ module Chatwoot
 
     # Custom chatwoot configurations
     config.x = config_for(:app).with_indifferent_access
-    config.action_dispatch.default_headers = {
+    config.x.action_dispatch.default_headers = {
   'Header-Name' => 'Header-Value',
   'X-Frame-Options' => 'ALLOWALL'
 }
+    
   end
 
   def self.config
